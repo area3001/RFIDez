c9276e83ddc97482ffb6784e20aa5b21d3f4da00
Bert Outtier <outtierbert@gmail.com>
2013-10-15 20:58:23 +0200

    initial commit

---
+++ yhy522/commands.py
@@ -0,0 +1,36 @@
+# System commands
+
+Test_Com = 0x00
+MSleep = 0x03
+MConfigure = 0x04
+Download_Keys = 0x05
+Download_Block_String = 0x08
+Download_Value = 0x09
+Antenna_Control = 0x11
+Sense_Mode = 0x13
+Beep = 0x14
+Beep_time = 0x15
+Output1 = 0x16
+Output2 = 0x17
+
+# RFID commands
+
+Change_Card_Keys = 0x06
+LOCK_Card = 0x07
+Card_Sleep = 0x12
+Card_Type = 0x19
+Card_ID = 0x20
+Block_Read = 0x21
+Block_Write = 0x22
+Value_Init = 0x23
+Value_Read = 0x24
+Value_Inc = 0x25
+Value_Dec = 0x26
+Value_Backup = 0x27
+Sector_Read = 0x2a
+Sector_Write = 0x2b
+
+
+
+
+
---
+++ yhy522/yhy522.py
@@ -0,0 +1,78 @@
+import serial
+import commands
+
+HEADER = 0xAABB
+
+# XOR of Length and Data bytes
+def calculate_checksum(length, command, data):
+    result = length ^ command
+    result = result ^ data
+    return result
+
+def send_command(command, data):
+    conn = serial.Serial('/dev/ttyAM0', 19200, timeout=1)
+    length = len(hex(data))
+    csum = calculate_checksum(length, command, data)
+    conn.write(HEADER + length + data + csum)
+    print("{0} {1} {2} {3} {4}".format(hex(HEADER), hex(command), hex(length), hex(data), hex(csum)))
+    line = conn.readline()   # read a '\n' terminated line
+    print("returned : {1}".format(line))
+    conn.close()
+    return line
+
+# System commands
+def Test_Com(data):
+    send_command(commands.Test_Com, data) 
+def MSleep(data):
+    send_command(commands.MSleep, data)
+def MConfigure(data):
+    send_command(commands.MConfigure, data)
+def Download_Keys(data):
+    send_command(commands.Download_Keys, data)
+def Download_Block_String(data):
+    send_command(commands.Download_Block_String, data)
+def Download_Value(data):
+    send_command(commands.Download_Value, data)
+def Antenna_Control(data):
+    send_command(commands.Antenna_Control, data)
+def Sense_Mode(data):
+    send_command(commands.Sense_Mode, data)
+def Beep(data):
+    send_command(commands.Beep, data)
+def Beep_time(data):
+    send_command(commands.Beep_time, data)
+def Output1(data):
+    send_command(commands.Output1, data)
+def Output2(data):
+    send_command(commands.Output2, data)
+
+# RFID commands
+
+def Change_Card_Keys(data):
+    send_command(commands.Change_Card_Keys, data)
+def LOCK_Card(data):
+    send_command(commands.LOCK_Card, data)
+def Card_Sleep(data):
+    send_command(commands.Card_Sleep, data)
+def Card_Type(data):
+    send_command(commands.Card_Type, data)
+def Card_ID(data):
+    send_command(commands.Card_ID, data)
+def Block_Read(data):
+    send_command(commands.Block_Read, data)
+def Block_Write(data):
+    send_command(commands.Block_Write, data)
+def Value_Init(data):
+    send_command(commands.Value_Init, data)
+def Value_Read(data):
+    send_command(commands.Value_Read, data)
+def Value_Inc(data):
+    send_command(commands.Value_Inc, data)
+def Value_Dec(data):
+    send_command(commands.Value_Dec, data)
+def Value_Backup(data):
+    send_command(commands.Value_Backup, data)
+def Sector_Read(data):
+    send_command(commands.Sector_Read, data)
+def Sector_Write(data):
+    send_command(commands.Sector_Write, data)
