370a999649944f993f57efdeb7286821164b0ad6
Bert Outtier <outtierbert@gmail.com>
2013-11-26 19:25:13 +0100

    changed main test method

--- src/example.py
+++ src/example.py
@@ -10,10 +10,13 @@ import yhy522
 
 def main(argv):
 #     print '0x%.2x' % yhy522.calculate_checksum(0x03, [0x01,0x02,0x03])
-    print "Test_Com(0x44)"
-    yhy522.Test_Com([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])
-    print "Card_ID()"
-    yhy522.Card_ID()
+    print "Test_Com([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])"
+    if(yhy522.Test_Com([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])):
+    	print "Test_Com succes"
+    else:
+    	print "Test_Com failed"
+    # print "Card_ID()"
+    # yhy522.Card_ID()
 #     yhy522.Card_ID(data)
 
 if __name__ == "__main__":
